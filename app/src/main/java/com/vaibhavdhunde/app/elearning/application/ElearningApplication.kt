05730161b9b3a9fcb8037d5aecd6241555780861
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2020-02-01 16:27:50 +0530

    Add ElearningApplication

---
+++ app/src/main/java/com/vaibhavdhunde/app/elearning/application/ElearningApplication.kt
@@ -0,0 +1,37 @@
+package com.vaibhavdhunde.app.elearning.application
+
+import android.app.Application
+import com.vaibhavdhunde.app.elearning.api.ElearningApi
+import com.vaibhavdhunde.app.elearning.api.NetworkInterceptor
+import com.vaibhavdhunde.app.elearning.data.DefaultElearningRepository
+import com.vaibhavdhunde.app.elearning.data.ElearningRepository
+import com.vaibhavdhunde.app.elearning.data.UsersLocalDataSource
+import com.vaibhavdhunde.app.elearning.data.UsersRemoteDataSource
+import com.vaibhavdhunde.app.elearning.data.source.local.ElearningDatabase
+import com.vaibhavdhunde.app.elearning.data.source.local.IUsersLocalDataSource
+import com.vaibhavdhunde.app.elearning.data.source.remote.IUsersRemoteDataSource
+import com.vaibhavdhunde.app.elearning.util.ViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class ElearningApplication : Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@ElearningApplication))
+        bind() from singleton { NetworkInterceptor(instance()) }
+        bind() from singleton { ElearningApi(instance()) }
+        bind() from singleton { ElearningDatabase.getInstance(instance()) }
+        bind() from singleton { instance<ElearningDatabase>().usersDao() }
+        bind<UsersLocalDataSource>() with singleton { IUsersLocalDataSource(instance()) }
+        bind<UsersRemoteDataSource>() with singleton { IUsersRemoteDataSource(instance()) }
+        bind<ElearningRepository>() with singleton {
+            DefaultElearningRepository(instance(), instance())
+        }
+        bind() from provider { ViewModelFactory(instance()) }
+    }
+}
---
+++ app/src/main/java/com/vaibhavdhunde/app/elearning/util/ViewModelFactory.kt
@@ -0,0 +1,26 @@
+package com.vaibhavdhunde.app.elearning.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.vaibhavdhunde.app.elearning.data.ElearningRepository
+import com.vaibhavdhunde.app.elearning.ui.login.LoginViewModel
+import com.vaibhavdhunde.app.elearning.ui.register.RegisterViewModel
+import com.vaibhavdhunde.app.elearning.ui.splash.SplashViewModel
+
+class ViewModelFactory(private val repository: ElearningRepository) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return with(modelClass) {
+            when {
+                isAssignableFrom(SplashViewModel::class.java) ->
+                    SplashViewModel(repository)
+                isAssignableFrom(LoginViewModel::class.java) ->
+                    LoginViewModel(repository)
+                isAssignableFrom(RegisterViewModel::class.java) ->
+                    RegisterViewModel(repository)
+                else -> throw IllegalArgumentException("Unknown model class: $modelClass")
+            }
+        } as T
+    }
+}
